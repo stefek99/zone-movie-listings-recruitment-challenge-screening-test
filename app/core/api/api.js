04653e9bb46cd1de7efc61748ff9df146fe932ae
Mars Robertson <>
2019-01-05 13:34:39 +0100

    adding API service and call to genres

---
+++ app/core/api/api.js
@@ -0,0 +1,17 @@
+angular.module('myApp.api', [])
+  
+  .value('key', 'fd89eaca88c8dfe61da3335a0b5b1c49')
+  .value('base', 'https://api.themoviedb.org/3')
+
+  .service('api', ['key', 'base', '$http', function(key, base, $http) {
+    let service = {};
+
+    service.genres = () => {
+        let path = '/genre/movie/list'
+
+        return $http.get(`${base}${path}?api_key=${key}`);
+    }
+
+    return service;
+  }])
+  
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'myApp.api'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
@@ -9,6 +9,14 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', ['$scope', 'api', function($scope, api) { // cannot use fat arrow here: https://github.com/angular/angular.js/issues/14814#issuecomment-228083403
 
-}]);
+  $scope.rating = 3;
+
+  $scope.search = function() {
+    api.genres().then((actual) => {
+      console.log(actual);
+    })
+  }
+
+}]);  
